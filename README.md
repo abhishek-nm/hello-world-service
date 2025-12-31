d8d3b6f57a317035f93c0bced2cc8a8e30c268e2
Abhishek <abhishek@Smritis-Laptop.local>
2025-12-31 23:16:02 +0530

    Initial commit - service A

---
+++ README.md
@@ -0,0 +1,5 @@
+Service A
+
+Calls Service B internally using Kubernetes DNS.
+Deployed via GitOps using Argo CD.
+
---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    r = requests.get("http://hello-world-1:5000/internal")
+    return f"Service A → {r.text}"
+
+app.run(host="0.0.0.0", port=5000)
+
