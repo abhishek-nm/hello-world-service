bd0f746a490da98084f931c696cb9fd1e5968b37
Abhishek Kumar <abhishekemail01@gmail.com>
2026-01-01 21:56:02 +0530

    sencond edit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     r = requests.get("http://hello-world-1:5000/internal")
-    return f"Service A →after commmit {r.text}"
+    return f"Service A →after commmit one more time {r.text}"
 
 app.run(host="0.0.0.0", port=5000)
 
