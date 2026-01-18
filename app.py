52812f16a83ca5631c7f75b42b7164f172e532a0
Abhishek Kumar <abhishekemail01@gmail.com>
2026-01-18 21:07:44 +0530

    Update response message in hello route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     r = requests.get("http://hello-world-1:5000/internal")
-    return f"Service A →after commmit one more time {r.text}"
+    return f"Service A →this is the response generated while demoing {r.text}"
 
 app.run(host="0.0.0.0", port=5000)
 
